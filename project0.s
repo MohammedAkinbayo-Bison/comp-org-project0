5fdf90e14eb02fdadc07fae2038d7d829c9f8232
MohammedAkinbayo-Bison <mohammed.akinbayo@bison.howard.edu>
2022-03-04 22:52:06 -0500

    Edit loop and main block. Now string allocation is in loop block

--- project0.s
+++ project0.s
@@ -1,15 +1,18 @@
 # Project 0: Hello World
 .data
-    s_out:  .asciiz "Hello @02966650\n"
+    s_out:  .asciiz "Hello @02966650"
+    n:      .asciiz "\n"
 .text
 main:
     li $s0, 5                   # 2966650 % 11 = 5
     li $v0, 4                   # sytem code for printing
-    la $a0, s_out               # load s_out string into register a0
-    li $t0, 0               # counter for loop (initialized with zero)
+    li $t0, 0                   # counter for loop (initialized with zero)
 
 loop:
+    la $a0, s_out               # load s_out string into register a0
+    syscall                     # print s_out
     beq $t0, $s0, exit          # if loop counter == 5, exit loop
+    la $a0, n                   # load s_out string into register a0
     syscall                     # print s_out
     addi $t0, $t0, 1            # loop counter ++
     j loop
